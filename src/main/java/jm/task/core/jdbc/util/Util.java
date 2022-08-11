49d6612c14a39febbe5912573121e5742346eb7b
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-11 08:44:32 +0300

    This is the solution to task 1.1.3
    Pre-project

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+// реализуйте здесь настройку соеденения с БД (ТЗ)
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+        private final static String driver = "com.mysql.cj.jdbc.Driver";
+        static final String url = "jdbc:mysql://localhost:3306/jdbc_project_db";
+        static final String username = "mvkola";
+        static final String password = "lXJD@ge1";
+        static Connection connection;
+
+        public static Connection getConnection() {
+                try {
+                        Class.forName(driver);
+                        connection = DriverManager.getConnection(url, username, password);
+                        System.out.println("INFO: Connected successfully");
+                } catch (SQLException | ClassNotFoundException e) {
+                        Logger.getLogger("connect").log(Level.WARNING, "Connection failed...");
+                }
+                return connection;
+        }
 }
