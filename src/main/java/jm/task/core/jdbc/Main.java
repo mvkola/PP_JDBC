77cd414d2731cd53906be87460820c3d186f7f58
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-11 08:47:01 +0300

    This is the solution to task 1.1.3
    Pre-project

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserService userService = new UserServiceImpl();    // слой Service отвечает за бизнес-логику
+        userService.createUsersTable();                     // здесь используем только методы слоя Service
+        
+        userService.saveUser("Alex", "Pushkin", (byte) 37);
+        userService.saveUser("Anton", "Chekhov", (byte) 35);
+        userService.saveUser("Lev", "Tolstoy", (byte) 70);
+        userService.saveUser("Vladimir", "Nabokov", (byte) 55);
+        
+        userService.removeUserById(3);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
